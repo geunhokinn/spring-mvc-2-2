26eb756d5ef3893b4cd772683480ef3b44bd5c8d
geunhokinn <gghh1220@gmail.com>
2024-05-02 20:20:05 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/hello/itemservicev2/ItemServiceV2Application.java
@@ -0,0 +1,13 @@
+package hello.itemservicev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/itemservicev2/ItemServiceV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.itemservicev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
